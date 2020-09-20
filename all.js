c330014395cc4c03576b05034caa8eef962d4676
cynthia <cynthia870@gmail.com>
2020-09-20 12:42:11 +0800

    add js feature 2

--- all.js
+++ all.js
@@ -2,4 +2,11 @@ for(var i=3;i<=15;i++){
     for(var y=1;y<=15;y++){
       console.log(i+'x'+y+'='+i*y);
     }
-  }
+}
+
+var x =178;
+var y = 70;
+var h = x/100;
+var w= y;
+var total = w/(h*h);
+console.log(Math.floor(total*10)/10);
